ee5ad653ab37df8cb98bc867ea170cd155f73735
Arif Wahyu <arifwahyu696@gmail.com>
2024-11-29 20:38:00 +0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        darkPrimary: '#1D1F21',
-        darkSecondary: '',
-        greenPrimary: '#B7F843'
+        transparent: "transparent",
+        darkPrimary: "#1C2021",
+        darkSecondary: "#313131",
+        greenPrimary: "#B7F843",
       },
     },
   },
