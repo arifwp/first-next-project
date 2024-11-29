c7ecfc146e51d513c890376c59ce171174bd0d1e
Arif Wahyu <arifwahyu696@gmail.com>
2024-11-29 20:38:15 +0700

    add data for header

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,56 +1,62 @@
 interface NavInterface {
-    id:number;
-    label:string;
-    link:string;
+  id: number;
+  label: string;
+  link: string;
 }
 
 export const navsList: NavInterface[] = [
-    {
-        id: 1,
-        label: 'Beranda',
-        link: '#beranda'
-    },
-    {
-        id: 2,
-        label: 'Journey',
-        link: '#journey'
-    },
-    {
-        id: 3,
-        label: 'Skills',
-        link: '#skills'
-    },
-    {
-        id: 4,
-        label: 'Achievements',
-        link: '#achievements'
-    },
-]
+  {
+    id: 1,
+    label: "Beranda",
+    link: "#beranda",
+  },
+  {
+    id: 2,
+    label: "Journey",
+    link: "#journey",
+  },
+  {
+    id: 3,
+    label: "Skills",
+    link: "#skills",
+  },
+  {
+    id: 4,
+    label: "Achievements",
+    link: "#achievements",
+  },
+];
 
 interface JourneyInterface {
-    id:number;
-    title:string;
-    description:string;
-    image:string;
+  id: number;
+  title: string;
+  description: string;
+  image: string;
 }
 
-export const journeyList:JourneyInterface[] = [
-    {
-        id:1,
-        title:"Gym",
-        description:"",
-        image: '',
-    },
-    {
-        id:2,
-        title:"Yoga",
-        description:"",
-        image: '',
-    },
-    {
-        id:3,
-        title:"Martial Art",
-        description:"",
-        image: '',
-    },
-]
+export const journeyList: JourneyInterface[] = [
+  {
+    id: 1,
+    title: "Gym",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+    image:
+      "https://plus.unsplash.com/premium_photo-1669446008800-9a124b0fd3a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Yoga",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+    image:
+      "https://plus.unsplash.com/premium_photo-1669446008800-9a124b0fd3a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Martial Art",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+    image:
+      "https://plus.unsplash.com/premium_photo-1669446008800-9a124b0fd3a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
