9fdaca351c02e66a26e22ca282265883d0b4914b
Arif Wahyu <arifwahyu696@gmail.com>
2024-12-10 22:41:54 +0700

    data for achievement

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -60,3 +60,65 @@ export const journeyList: JourneyInterface[] = [
       "https://plus.unsplash.com/premium_photo-1669446008800-9a124b0fd3a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+
+interface AchievementInterface {
+  id: number;
+  name: string;
+  image: string;
+  studentOf: string;
+  rate: string;
+  description: string;
+}
+
+export const achievementList: AchievementInterface[] = [
+  {
+    id: 1,
+    name: "Skiva Millers",
+    image:
+      "https://plus.unsplash.com/premium_photo-1677283510950-54741b2c305f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    studentOf: "Yoga Student",
+    rate: "4.5",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+  },
+  {
+    id: 2,
+    name: "Mia Khalifa",
+    image:
+      "https://plus.unsplash.com/premium_photo-1664379519132-aa97377491ca?q=80&w=3110&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    studentOf: "Cardio Exercise Student",
+    rate: "4.2",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+  },
+  {
+    id: 3,
+    name: "Lena Paul",
+    image:
+      "https://plus.unsplash.com/premium_photo-1664359679216-60ffd478201a?q=80&w=3086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    studentOf: "Swimming Student",
+    rate: "4",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+  },
+  {
+    id: 4,
+    name: "Rae Lil Black",
+    image:
+      "https://images.unsplash.com/photo-1631355632079-349f2b97447a?q=80&w=2850&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    studentOf: "Model Student",
+    rate: "4.8",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+  },
+  {
+    id: 5,
+    name: "Magdalena",
+    image:
+      "https://plus.unsplash.com/premium_photo-1667900670427-5819c447e00e?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    studentOf: "Black Belt Student",
+    rate: "4.8",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+  },
+];
