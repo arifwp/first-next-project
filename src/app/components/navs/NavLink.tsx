f87fd05f8e1329537b79dbce5d5e3e97211e4fdd
Arif Wahyu <arifwahyu696@gmail.com>
2024-12-11 22:25:53 +0700

    change to a

--- src/app/components/navs/NavLink.tsx
+++ src/app/components/navs/NavLink.tsx
@@ -1,9 +1,15 @@
+import { NavInterface } from "@/app/utils/constants";
+
 interface NavLinkProps {
-  label: string;
+  item: NavInterface;
 }
 
-const NavLink = ({ label }: NavLinkProps) => {
-  return <button className="text-xs text-white-600">{label}</button>;
+const NavLink = ({ item }: NavLinkProps) => {
+  return (
+    <a href={`${item.link}`} className="text-xs text-white-600">
+      {item.label}
+    </a>
+  );
 };
 
 export default NavLink;
