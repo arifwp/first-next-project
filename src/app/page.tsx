ab7cdd199eea809eba94867d9d74ad1142e7a596
Arif Wahyu <arifwahyu696@gmail.com>
2024-12-11 22:25:30 +0700

    add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 import Achievement from "./components/Achievement";
 import Beranda from "./components/Beranda";
+import Closing from "./components/Closing";
 import Journey from "./components/Journey";
 import SkillsView from "./components/SkillsView";
+import SubscriptionPlans from "./components/SubscriptionPlans";
 import Works from "./components/Works";
 
 export default function Home() {
   return (
     <>
       <Beranda />
+
       <Journey />
+
       <Works />
+
       <SkillsView />
+
       <Achievement />
+
+      <SubscriptionPlans />
+
+      <Closing />
     </>
   );
 }
